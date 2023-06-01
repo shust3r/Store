5a38dd71e9eb438321e7a160a98a5a08c8d49fac
Yardo <yardonesenchuk@icloud.com>
2023-06-01 13:45:16 +0300

    Created first model, added DbContext

---
+++ Store/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Store.Models;
+
+namespace Store.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+               
+        }
+
+        public DbSet<Category> Category { get; set; }
+    }
+}
