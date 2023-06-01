30bfe529b377aa9f6270503c6b6edf33dde80764
Yardo <yardonesenchuk@icloud.com>
2023-06-01 14:41:25 +0300

    Implemented "scratch" of Category.Index Controller and it's View

---
+++ Store/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Store.Data;
+using Store.Models;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace Store.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objList = _db.Category;
+            return View(objList);
+        }
+    }
+}
