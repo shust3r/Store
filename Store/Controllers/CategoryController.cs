1fa4cf9cc3ab6814ccb3e34da7ef1129577abf9d
Yardo <yardonesenchuk@icloud.com>
2023-06-01 15:45:25 +0300

    Implemented Get-type action method Create; Implemented Post-type action method Create in order to add new objects to the list

--- Store/Controllers/CategoryController.cs
+++ Store/Controllers/CategoryController.cs
@@ -19,5 +19,21 @@ namespace Store.Controllers
             IEnumerable<Category> objList = _db.Category;
             return View(objList);
         }
+
+        //GET - Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST - Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            _db.Category.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
