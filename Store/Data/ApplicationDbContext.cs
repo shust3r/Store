5c574d3418b9fc55802cdb57ef052241b5a5be58
Yardo <yardonesenchuk@icloud.com>
2023-06-02 10:47:22 +0300

    New model Product was added to Database

--- Store/Data/ApplicationDbContext.cs
+++ Store/Data/ApplicationDbContext.cs
@@ -12,5 +12,6 @@ namespace Store.Data
 
         public DbSet<Category> Category { get; set; }
         public DbSet<ApplicationType> ApplicationType { get; set; }
+        public DbSet<Product> Product { get; set; }
     }
 }
