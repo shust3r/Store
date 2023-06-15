e8f9588dacfbac2fca01c97e9f41cfdddadc7a3c
Yardo <yardonesenchuk@icloud.com>
2023-06-15 16:44:00 +0300

    ViewModel for order list management was added

---
+++ Store/Controllers/OrderController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Store_DataAccess.Repository.IRepository;
+using Store_Models.ViewModels;
+using Store_Utility.BrainTree;
+
+namespace Store.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IOrderHeaderRepository _orderHRepo;
+        private readonly IOrderDetailRepository _orderDRepo;
+        private readonly IBrainTreeGate _brain;
+
+        public OrderController(IOrderHeaderRepository orderHRepo, IOrderDetailRepository orderDRepo, IBrainTreeGate brain)
+        {
+            _orderHRepo = orderHRepo;
+            _orderDRepo = orderDRepo;
+            _brain = brain;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
